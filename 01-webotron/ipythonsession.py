43c7ea20295b7eb57e70021b6645d19ff6b2974d
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2019-04-02 12:07:56 +0530

    setup project directory for webotron

---
+++ 01-webotron/ipythonsession.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print (bucket)
+    
+'test'
+ec2_client = session.client('ec2')
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('save', 'ipythonsession.py')
+get_ipython().run_line_magic('save', 'ipythonsession.py 1-10')
